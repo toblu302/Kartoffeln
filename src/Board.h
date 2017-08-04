621264e9bc4ebbd3155a582ebb0d66a782bd7ffc
toblu302 <toblu302@users.noreply.github.com>
2017-08-04 19:56:36 +0200

    Add the header-file for the board-struct

---
+++ src/Board.h
@@ -0,0 +1,24 @@
+#ifndef BOARD_H_INCLUDED
+#define BOARD_H_INCLUDED
+
+#include <stdint.h>
+
+#define NUM_PIECES 6
+enum PIECE {
+    PAWN=0, ROOK=1, KNIGHT=2, BISHOP=3, QUEEN=4, KING=5
+};
+
+#define NUM_COLORS 2
+enum COLOR {
+    WHITE=0, BLACK=1
+};
+
+struct Board {
+    uint64_t pieces[NUM_PIECES];
+    uint64_t color[NUM_COLORS];
+
+    uint64_t EN_PASSANT_SQUARE;
+    uint64_t CASTLE_RIGHTS;
+};
+
+#endif // BOARD_H_INCLUDED
